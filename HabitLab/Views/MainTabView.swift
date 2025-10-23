6707e3cc7622d716ff01b9f2717befb3f46e0df1
sofiarphansdi <sofiarphansdi@icloud.com>
2025-10-23 16:05:01 +0500

    Update MainTabView.swift

--- HabitLab/Views/MainTabView.swift
+++ HabitLab/Views/MainTabView.swift
@@ -12,7 +12,6 @@ struct MainTabView: View {
     
     var body: some View {
         TabView {
-            /*
             CalendarView()
                 .environmentObject(habitViewModel)
                 .tabItem {
@@ -34,7 +33,6 @@ struct MainTabView: View {
                 .tabItem {
                     Label("Profile", systemImage: "person.fill")
                 }
-             */
         }
     }
 }
